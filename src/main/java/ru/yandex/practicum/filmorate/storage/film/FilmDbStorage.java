2228f29fb2d4a995eb26018a00878342280ba029
eklerkjke <eklerkjke@gmail.com>
2024-11-28 18:30:33 +0500

    Добавил функционал общих фильмов

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -79,12 +79,9 @@ public class FilmDbStorage extends BaseStorage<Film> implements FilmStorage {
             LEFT JOIN mpa_type AS m_type ON film.mpa = m_type.mpa_id
             LEFT JOIN directors_films AS d_film ON film.film_id = d_film.film_id
             LEFT JOIN directors AS dir ON d_film.dir_id = dir.dir_id
-            
             JOIN likes AS first_likes ON film.film_id = first_likes.film_id
             JOIN likes AS second_likes ON film.film_id = second_likes.film_id
-            
             WHERE first_likes.user_id = ? AND second_likes.user_id = ?
-            
             GROUP BY film.film_id
             ORDER BY likes_count DESC;
             """;
